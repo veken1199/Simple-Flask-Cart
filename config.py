452ee5cfa0e73a27027b67f2029f3abe083f3579
veken1199 <super_veko@yahoo.com>
2019-01-20 13:18:58 -0500

    Improved session token

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class BaseConfig(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Session secret keys
-    SECRET_KEY = 'my key'
+    SECRET_KEY = 'ae24a87634bf4d749bef9d089cfcb79140d47727'
     SESSION_TYPE = 'sqlalchemy'
     SESSION_USE_SIGNER = True
 
